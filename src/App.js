af487b11896b6548d05dd3f87273972fcdbad856
sarbjit <admin@admin.com>
2023-06-30 12:57:15 +0530

    added alert

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import './App.css';
 import CardTable from './pages/CardTable';
 import Home from './pages/Home';
 
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
 
 
- 
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  // you can also just use 'scale'
+  transition: transitions.SCALE
+}
 
 
 function App() {
@@ -19,6 +28,7 @@ function App() {
 
   return (
     <>
+    <AlertProvider template={AlertTemplate} {...options}>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
@@ -26,6 +36,7 @@ function App() {
           <Route path="*" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      </AlertProvider>
     </>
   );
 }
